4116d81a644625f128681a39232f0b4479eb968b
José Javier Señaris <pepesenaris@gmail.com>
2015-07-29 14:29:52 -0400

    Allow CORS requests in backend 	To speed up development, this is an easy workround 	Should not be done in production

--- backend/app/server.js
+++ backend/app/server.js
@@ -5,6 +5,13 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get('/events', function (req, res) {
   res.json({events: db.findAll()});
 });
