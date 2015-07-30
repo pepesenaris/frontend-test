a882a41a7b83d5a7df05d1606ffcf6ff0d647b8b
José Javier Señaris <pepesenaris@gmail.com>
2015-07-30 17:36:03 -0400

    Test event show

--- frontend/app/scripts/components/event_show.jsx
+++ frontend/app/scripts/components/event_show.jsx
@@ -1,8 +1,33 @@
 var React = require('react');
+var EventStore = require('../stores/event_store');
 
 var EventShow = React.createClass({
+
+  getInitialState: function(){
+    console.log("Event show: ", this.props.params.id);
+    event = EventStore.getEvent(this.props.params.id);
+    return {
+      id: event.id,
+      name: event.title,
+      location: event.location,
+      description: event.description,
+      url: event.eventImage,
+      dates: event.dates
+    };
+  },
+
 	render: function(){
-		return (<div>"Hello event"</div>);
+		return (
+      <div>
+        Name: {this.state.name}
+        <br/>
+        "Location": {this.state.location}
+        <br/>
+        Description: {this.state.description}
+        <br/>
+        Url: {this.state.url}
+      </div>
+      );
 	}
 });
 
