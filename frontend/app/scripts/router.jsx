d7c954d48d0e67e4a6dc4106a948db4e9c8a3823
José Javier Señaris <pepesenaris@gmail.com>
2015-07-30 15:31:30 -0400

    Put back # in routes & add new routes

--- frontend/app/scripts/router.jsx
+++ frontend/app/scripts/router.jsx
@@ -6,16 +6,20 @@ var DefaultRoute = Router.DefaultRoute;
 var Layout = require('./components/layout');
 var Home = require('./components/home');
 var EventIndex = require('./components/event_index');
+var EventForm = require('./components/event_form');
+var EventShow = require('./components/event_show');
 
 var routes = (
-	<Route name="layout" path="/" handler={Layout}>
-		<DefaultRoute handler={EventIndex} />
+	<Route handler={Layout}>
+		<DefaultRoute handler={EventIndex}/>
+    <Route name="new" path="event/new" handler={EventForm}/>
+    <Route name="show" path="event/:id" handler={EventShow}/>
 	</Route>
 );
 
 exports.start = function() {
 
-  Router.run(routes, Router.HistoryLocation, function (Handler) {
+  Router.run(routes, function (Handler) {
 		React.render(<Handler />, document.getElementById('content'));
 	});
 }
