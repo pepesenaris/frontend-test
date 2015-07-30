209b61867c83edcadf2836871014fb3a081c716a
José Javier Señaris <pepesenaris@gmail.com>
2015-07-30 15:32:40 -0400

    Initial components for form & show

---
+++ frontend/app/scripts/components/event_form.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+
+var EventForm = React.createClass({
+	render: function(){
+		return (<div>"Hello form"</div>);
+	}
+});
+
+
+module.exports = EventForm;
---
+++ frontend/app/scripts/components/event_show.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+
+var EventShow = React.createClass({
+	render: function(){
+		return (<div>"Hello event"</div>);
+	}
+});
+
+
+module.exports = EventShow;
