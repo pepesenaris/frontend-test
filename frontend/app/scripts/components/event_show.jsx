0556dba47854f923ee97bf588f391f7c5eb96f90
José Javier Señaris <pepesenaris@gmail.com>
2015-07-30 22:46:17 -0400

    New layout for show

--- frontend/app/scripts/components/event_show.jsx
+++ frontend/app/scripts/components/event_show.jsx
@@ -18,14 +18,40 @@ var EventShow = React.createClass({
 
 	render: function(){
 		return (
-      <div>
-        Name: {this.state.name}
-        <br/>
-        "Location": {this.state.location}
-        <br/>
-        Description: {this.state.description}
-        <br/>
-        Url: {this.state.url}
+      <div className="event-container well clearfix">
+          <div className="col-sm-5 col-xs-12 nopadding event-image">
+              <img src={this.state.url} alt=""></img>
+          </div>
+          <div className="col-sm-6 col-xs-12 nopadding event-content">
+              <div className="event-date">
+                  <div className="blurb">
+                      {this.state.name}
+                  </div>
+                  <div className="event-location">
+                      <div className="address">
+                         {this.state.location}
+                      </div>
+                      <div className="clear-all"></div>
+                  </div>
+                  <hr />
+                  <div className="pull-left">
+                      <ul>
+                          <li>{this.state.dates[0].split(' ')[0]}</li>
+                          <li>{this.state.dates[0].split(' ')[1]}</li>
+                      </ul>
+                      <ul>
+                          <li>{this.state.dates[1].split(' ')[0]}</li>
+                          <li>{this.state.dates[1].split(' ')[1]}</li>
+                      </ul>
+                  </div>
+                  <div className="clear-all"></div>
+              </div>
+          </div>
+          <div className="clear-all"></div>
+          <hr />
+          <p className="general-text">
+              {this.state.description}
+          </p>
       </div>
       );
 	}
